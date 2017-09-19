ac4434de238da93df5b6a8f149975ee755197815
Filip Zych <30993768+Ashanen@users.noreply.github.com>
2017-09-19 17:51:31 +0200

    Add files via upload

---
+++ Stoper/Plansza.cpp
@@ -0,0 +1,19 @@
+#include "MyForm.h"
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+
+int main(array<System::String ^> ^args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Stoper::MyForm formarz;
+	Application::Run(%formarz);
+	return 0;
+}
+
+
+
+ 
